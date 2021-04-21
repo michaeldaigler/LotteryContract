81c4e5fd59481be09e4313419adf009220440236
Michael Daigler <mdaigler2601@gmail.com>
2021-04-21 09:30:51 -0400

    even more

--- Lottery.sol
+++ Lottery.sol
@@ -15,9 +15,22 @@ contract Lottery {
         players.push(msg.sender);
     }
 
+    function find_winner() public view returns(address){
+        require(msg.sender == manager);
+        uint r = random();
+        address  winner;
+        winner = players[r % players.length];
+        return winner;
+
+    }
+
     function get_balance() public view returns(uint) {
         require(msg.sender == manager);
         return address(this).balance;
+    }
+
+    function random() private view returns(uint256) {
+        return uint256(keccak256(abi.encode(block.difficulty, block.timestamp, players.length)));
     }
 
 }

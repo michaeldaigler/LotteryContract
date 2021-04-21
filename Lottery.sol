e8ad66ea5600c399782305bd9508ebeff28e6334
Michael Daigler <mdaigler2601@gmail.com>
2021-04-21 09:10:16 -0400

    More

--- Lottery.sol
+++ Lottery.sol
@@ -3,5 +3,19 @@ pragma solidity ^0.8.0;
 
 contract Lottery {
     address[] public players;
+    address public manager;
+
+    constructor()  {
+        manager = msg.sender;
+    }
+
+    receive () payable external {
+
+        players.push(msg.sender);
+    }
+
+    function get_balance() public view returns(uint) {
+        return address(this).balance;
+    }
 
 }
